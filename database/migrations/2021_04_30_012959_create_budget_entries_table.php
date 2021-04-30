887b31fd089084e9abff88473a170eda46258f88
thetravisweber <tbroweber@gmail.com>
2021-04-29 18:33:40 -0700

    added budget entries table

---
+++ database/migrations/2021_04_30_012959_create_budget_entries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBudgetEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('budget_entries', function (Blueprint $table) {
+            $table->id();
+            $table->string('field');
+            $table->int('value');
+            $table->foreignId('row_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('budget_entries');
+    }
+}
