b4512b43f5fca081e1470f0e6cd1f35d1b5da26f
thetravisweber <tbroweber@gmail.com>
2021-05-21 10:31:55 -0700

    test: list rows route is ok

--- tests/Feature/BudgetRowTest.php
+++ tests/Feature/BudgetRowTest.php
@@ -50,47 +50,47 @@ class BudgetRowTest extends TestCase
         $this->assertCount(1, BudgetRow::all());
     }
 
-    // public function test_budget_entries_are_created_when_a_row_is_set()
-    // {
-    //     $response = $this->post(BudgetTest::ADD_URL, BudgetTest::BASIC_ADD_REQUEST);
+    public function test_budget_entries_are_created_when_a_row_is_set()
+    {
+        $response = $this->post(BudgetTest::ADD_URL, BudgetTest::BASIC_ADD_REQUEST);
 
-    //     $response->assertOk();
+        $response->assertOk();
 
-    //     $first = Budget::first();
+        $first = Budget::first();
 
-    //     $request = [
-    //         'food' => 50,
-    //         'fun' => 25,
-    //         'Vacation Savings' => 10
-    //     ];
+        $request = [
+            'food' => 50,
+            'fun' => 25,
+            'Vacation Savings' => 10
+        ];
 
-    //     $response = $this->post("budget/$first->id/add-row", $request);
+        $response = $this->post("budget/$first->id/add-row", $request);
 
-    //     $response->assertOk();
+        $response->assertOk();
 
-    //     $this->assertCount(count($request), BudgetEntry::all());
-    // }
+        $this->assertCount(count($request), BudgetEntry::all());
+    }
 
-    // public function test_get_row_route_is_ok()
-    // {
-    //     $response = $this->post(BudgetTest::ADD_URL, BudgetTest::BASIC_ADD_REQUEST);
+    public function test_get_row_route_is_ok()
+    {
+        $response = $this->post(BudgetTest::ADD_URL, BudgetTest::BASIC_ADD_REQUEST);
 
-    //     $response->assertOk();
+        $response->assertOk();
 
-    //     $first = Budget::first();
+        $first = Budget::first();
 
-    //     $addRequest = [
-    //         'food' => 50,
-    //         'fun' => 25,
-    //         'Vacation Savings' => 10
-    //     ];
+        $addRequest = [
+            'food' => 50,
+            'fun' => 25,
+            'Vacation Savings' => 10
+        ];
 
-    //     $this->post("budget/$first->id/add-row", $addRequest);
+        $this->post("budget/$first->id/add-row", $addRequest);
 
-    //     $response = $this->get("budget/$first->id/get-row");
+        $response = $this->get("budget/$first->id/list-rows");
 
-    //     $response->assertOk();
-    // }
+        $response->assertOk();
+    }
 
     // public function test_get_row_is_not_empty()
     // {
